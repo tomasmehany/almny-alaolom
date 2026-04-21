679a33ba52e01564a9945d0a12fb4f1fb9980430
tomasmehany <tomasmehany@gmail.com>
2026-04-21 22:44:48 +0200

    Change button text from 'الدعم الفني' to 'الادمن'

--- app/page.tsx
+++ app/page.tsx
@@ -443,7 +443,7 @@ export default function Home() {
                 style={styles.footerLink}
                 onClick={() => window.open('https://wa.me/message/UKASWZCU5BNLN1?src=qr', '_blank')}
               >
-                الدعم الفني
+                الادمن
               </button>
             </div>
           </div>
